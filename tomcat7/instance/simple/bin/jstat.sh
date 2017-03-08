ddc6020ab051226d8788c0e37ae03e6c00763457
nationminu <nationminu@users.noreply.github.com>
2017-03-08 13:50:10 +0900

    Update jstat.sh

--- tomcat7/instance/simple/bin/jstat.sh
+++ tomcat7/instance/simple/bin/jstat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . ./env.sh
 
-ps -ef | grep java | grep "\[SERVER_NAME=$SERVER_NAME\]" | awk {'print "kill -9 " $2'} | sh -x
+ps -ef | grep java | grep "\[SERVER_NAME=$SERVER_NAME\]" | awk {'print "jstat -gcutil " $2 " 1000 0"'} | sh -x
