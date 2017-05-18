1bc008653c6b316d38d20f785750a25119a91668
nationminu <nationminu@users.noreply.github.com>
2017-05-18 18:33:56 +0900

    Update kill.sh

--- tomcat7/instance/simple/bin/kill.sh
+++ tomcat7/instance/simple/bin/kill.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-. ./env.sh
+BASEDIR=$(dirname "$0")
+. $BASEDIR/env.sh
+
+unset JAVA_OPTS
 
 ps -ef | grep java | grep "\[SERVER_NAME=$SERVER_NAME\]" | awk {'print "kill -9 " $2'} | sh -x
