25142699edeb467a6934b1d5b5dbc2ebfca09d9d
nationminu <nationminu@users.noreply.github.com>
2017-05-18 18:34:14 +0900

    Update jstat.sh

--- tomcat7/instance/simple/bin/jstat.sh
+++ tomcat7/instance/simple/bin/jstat.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-. ./env.sh
+BASEDIR=$(dirname "$0")
+. $BASEDIR/env.sh
+
+unset JAVA_OPTS
 
 ps -ef | grep java | grep "\[SERVER_NAME=$SERVER_NAME\]" | awk {'print "jstat -gcutil " $2 " 1000 0"'} | sh -x
